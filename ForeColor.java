7238c2cc6ab27d498d323efa623de5315fe323f9
Joelbu <89338010+Joelbu537@users.noreply.github.com>
2026-01-19 12:44:02 +0100

    Add files via upload

---
+++ ForeColor.java
@@ -0,0 +1,21 @@
+public class ForeColor {
+  private ForeColor() {}
+
+  public static final String RESET = "\033[0m";
+  public static final String BLACK = "\033[30m";
+  public static final String RED = "\033[38;5;001m";
+  public static final String GREEN = "\033[38;5;002m";
+  public static final String YELLOW = "\033[38;5;003m";
+  public static final String BLUE = "\033[38;5;004m";
+  public static final String PURPLE = "\033[38;5;005";
+  public static final String CYAN = "\033[38;5;006m";
+  public static final String LIGHT_GREY = "\033[38;5;007m";
+  public static final String GREY = "\033[38;5;008m";
+  public static final String LIGHT_RED = "\033[38;5;009m";
+  public static final String LIGHT_GREEN = "\033[38;5;010m";
+  public static final String LIGHT_YELLOW = "\033[38;5;011m";
+  public static final String LIGHT_BLUE = "\033[38;5;012m";
+  public static final String LIGHT_PURPLE = "\033[38;5;013m";
+  public static final String LIGHT_CYAN = "\033[38;5;014m";
+  public static final String WHITE = "\033[38;5;015m";
+}
